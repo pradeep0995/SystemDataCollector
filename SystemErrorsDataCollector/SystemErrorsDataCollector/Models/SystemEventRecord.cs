86da92ab28b7ad3bc181393247d05a4158b4cac8
Pradeep Yadav <pryada@microsoft.com>
2019-02-04 15:46:45 +0530

    Added models

---
+++ SystemErrorsDataCollector/SystemErrorsDataCollector/Models/SystemEventRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemErrorsDataCollector.Models
+{
+    class SystemEventRecord
+    {
+        public string  Level{ get; set; }
+        public string  Source { get; set; }
+        public string Time_Created { get; set; }
+        public int EventId { get; set; }
+        public string Message { get; set; }
+    }
+}
